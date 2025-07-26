24c4c24404aa9f48b47986bc31c9c815e63eb445
lihi.t <lihitoledan@gmail.com>
2025-07-26 21:50:09 +0300

    test

---
+++ preprocessor.c
@@ -0,0 +1,5 @@
+//
+// Created by lihi.t on 26/07/2025.
+//
+
+#include "preprocessor.h"
---
+++ preprocessor.h
@@ -0,0 +1,8 @@
+//
+// Created by lihi.t on 26/07/2025.
+//
+
+#ifndef PROJECT_PREPROCESSOR_H
+#define PROJECT_PREPROCESSOR_H
+
+#endif //PROJECT_PREPROCESSOR_H
