541ff2005f2e2845ab40120047a42c4d4761b8f9
lihi.t <lihitoledan@gmail.com>
2025-07-26 22:28:31 +0300

    add function trim_spaces

--- utils.c
+++ utils.c
@@ -1,1 +1,20 @@
-#include <stdio.h>
+#include "utils.h"
+
+
+
+char* trim_spaces(char* str) {
+    char* end;
+    /* Skip leading whitespace */
+    while (isspace(*str)) str++;
+
+    if (*str == '\0') return str;
+
+    /* Find end of string and trim trailing whitespace */
+    end = str + strlen(str) - 1;
+    while (end > str && isspace(*end)) {
+        *end = '\0';
+        end--;
+    }
+
+    return str;
+}
