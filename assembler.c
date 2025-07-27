2a3b5fb81c71dd2cad64522c5bfc69816d946713
lihi.t <lihitoledan@gmail.com>
2025-07-27 16:07:40 +0300

    errors structure

--- assembler.c
+++ assembler.c
@@ -1,6 +1,15 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "globals.h"
+#include "errors_handler.h"
+#include "first_pass.h"
 
 int main()
 {
+
+
     return 0;
 }
---
+++ second_pass.c
@@ -0,0 +1,5 @@
+//
+// Created by lihi.t on 27/07/2025.
+//
+
+#include "second_pass.h"
