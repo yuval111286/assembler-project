790caf0135f5a9aec72a3b41d49a38cb10cdbc89
lihi.t <lihitoledan@gmail.com>
2025-07-27 20:20:39 +0300

    test

--- analyze_text.c
+++ analyze_text.c
@@ -1,1 +1,28 @@
 #include "analyze_text.h"
+
+
+char* trim_spaces(char* str) {
+    char* end;
+    /* Skip leading whitespace */
+    while (isspace(*str)) str++;
+
+    if (*str == '\0') return str;
+
+    /* Find end of string and trim trailing whitespace */
+    end = str + strlen(str) - 1;
+    while (end > str && isspace(*end)) {
+        *end = '\0';
+        end--;
+    }
+
+    return str;
+}
+
+
+int check_and_skip_comment_or_empty_line(char *line) {
+    /* Checks if the line is a comment or empty */
+    if (*line == ';' || *line == '\0' || *line == '\n') {
+        return 1;
+    }
+    return 0;
+}
--- utils.c
+++ utils.c
@@ -1,24 +1,5 @@
 #include "utils.h"
 
-
-
-char* trim_spaces(char* str) {
-    char* end;
-    /* Skip leading whitespace */
-    while (isspace(*str)) str++;
-
-    if (*str == '\0') return str;
-
-    /* Find end of string and trim trailing whitespace */
-    end = str + strlen(str) - 1;
-    while (end > str && isspace(*end)) {
-        *end = '\0';
-        end--;
-    }
-
-    return str;
-}
-
 void *malloc_allocation(size_t size) {
     void *ptr = malloc(size);
     if (ptr == NULL) {
