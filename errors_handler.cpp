480e9655de1f4a190150dcc2993f7214299d66e7
lihi.t <lihitoledan@gmail.com>
2025-07-27 13:35:34 +0300

    add files and main function

--- errors_handler.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by lihi.t on 26/07/2025.
-//
-
-#include "errors_handler.h"
---
+++ linked_list_struct.h
@@ -0,0 +1,57 @@
+
+
+#ifndef ASSEMBLER_PROJECT_LINKED_LIST_STRUCT_H
+#define ASSEMBLER_PROJECT_LINKED_LIST_STRUCT_H
+
+
+
+
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+
+/*
+ * node in a linked list for macros
+ * containing:
+ * macro name
+ * macro text - between macro to mcroend
+ * number of line
+ * pointer to the next node in the list
+ */
+typedef struct node{
+    char *name;
+    char *text;
+    int line;
+    struct node *next;
+} node;
+
+
+/*
+ * Receive
+ * Return new pointer of node
+ */
+node *create_node(char *name, char *content, int line_num);
+
+/*
+ * Receive
+ * Return new pointer of node
+ */
+void add_to_list(node **head, ...);
+
+/*
+ * Receive
+ * Return new pointer of node
+ */
+node *search_list(node *head, char *name, int *found);
+
+/*
+ * Receive
+ * Return new pointer of node
+ */
+void free_list(node *head);
+
+
+
+
+
+#endif //ASSEMBLER_PROJECT_LINKED_LIST_STRUCT_H
