eb74bbed1b03c59907ff738db04ce4a022993b18
lihi.t <lihitoledan@gmail.com>
2025-07-31 20:19:42 +0300

    second_pass declerations

--- second_pass.h
+++ second_pass.h
@@ -1,4 +1,24 @@
 #ifndef ASSEMBLER_PROJECT_SECOND_PASS_H
 #define ASSEMBLER_PROJECT_SECOND_PASS_H
 
+
+
+void add_entry_label_to_symbol_table();
+
+void add_extern_label_to_symbol_table();
+
+int binary_line_to_base_4(int binary_line, char *code);
+
+char *two_binary_digits_one_base4_char (int binary_num);
+
+void create_obj_file();
+
+void create_ent_file();
+
+void create_ext_file();
+
+
+
+
+
 #endif /*ASSEMBLER_PROJECT_SECOND_PASS_H*/
