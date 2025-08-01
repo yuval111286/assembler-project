b41b0aed0413cb3c7aee205f788042293221a6c8
lihi.t <lihitoledan@gmail.com>
2025-08-01 16:36:31 +0300

    change returned value for first_pass and build main function

---
+++ assembler_prog.c
@@ -0,0 +1,49 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+#include "errors_handler.h"
+#include "first_pass.h"
+#include "second_pass.h"
+#include "preprocessor.h"
+
+int main(int argc, char *argv[])
+{
+
+    char *as_file, *am_file;
+    SymbolTable symbol_table;
+    int IC_final, DC_final;
+
+
+    while (--argc > 0){
+
+        as_file = change_ending_of_file(argv[argc],".as");
+
+        printf("-- PREPROCESSOR --\n");
+        if (preprocessor_full_flow(as_file)) {
+            return 1;
+        }
+
+        init_symbol_table(&symbol_table);
+        IC_final = 0;
+        DC_final = 0;
+        am_file = change_ending_of_file(argv[argc], ".am");
+
+        printf("-- FIRST PASS --\n");
+
+        if (first_pass(am_file,&symbol_table, &IC_final, &DC_final)){
+            return 1;
+        }
+
+        printf("-- SECOND PASS --\n");
+
+        if (second_pass(as_file)) {
+            return 1;
+        }
+
+    }
+
+
+    return 0;
+}
--- preprocessor.h
+++ preprocessor.h
@@ -14,7 +14,7 @@
  *   extract mcro name and text and replace marco appearance by it's name
  *
  * @param file_name as file name
- * @return -1 if preprocessor fail and 0 if succeed
+ * @return 0 if preprocessor succeed or 1 if fail
  */
 int preprocessor_full_flow(char *file_name);
 
