4667c073bd42a9a1c70c78eb6a7bc956ecff612c
lihi.t <lihitoledan@gmail.com>
2025-08-07 18:04:35 +0300

    debug second pass

--- second_pass.c
+++ second_pass.c
@@ -31,7 +31,7 @@ char *turn_line_to_base_4(unsigned int word) {
 }
 
 char *turn_address_to_base_4(unsigned int address) {
-    static char address_is_coded_in_base4[5] = {0};         /* 5 digits + null terminator */
+    static char address_is_coded_in_base4[5] = {0};
     char base4_options[4] = {'a', 'b', 'c', 'd'};
     int i, remainder;
 
@@ -52,8 +52,6 @@ char *turn_address_to_base_4(unsigned int address) {
     return address_is_coded_in_base4;
 }
 
-
-
 int is_label_operand(char *operand) {
     /*check if not num  */
     if (operand[0] == '#') return 0;
@@ -67,7 +65,6 @@ int is_label_operand(char *operand) {
     /* return 1 for label */
     return 1;
 }
-
 
 void add_extern_symbol(ExternList *extern_list, char *symbol_name, int address) {
     ExternSymbol *new_node;
@@ -88,7 +85,6 @@ void add_extern_symbol(ExternList *extern_list, char *symbol_name, int address) 
     extern_list->head = new_node;
 }
 
-/* Free extern symbol list using temp node*/
 void free_extern_list(ExternList *extern_list) {
     ExternSymbol *current = extern_list->head;
     ExternSymbol *temp;
@@ -102,7 +98,6 @@ void free_extern_list(ExternList *extern_list) {
 
     extern_list->head = NULL;
 }
-
 
 void write_code_image_to_ob_file(CodeImage *img, int ic_size, int dc_size, unsigned int *data_image, char *file_name) {
     FILE *fp;
@@ -120,21 +115,17 @@ void write_code_image_to_ob_file(CodeImage *img, int ic_size, int dc_size, unsig
 
     /* go over code image section and code address and word*/
     for (i = 0; i < (*img).size; i++) {
-
         fprintf(fp, "%s %s\n", turn_address_to_base_4((*img).words[i].address), turn_line_to_base_4((*img).words[i].value));
     }
 
     /* go over data image section code address and word*/
     for (i = 0; i < dc_size; i++) {
-
         fprintf(fp, "%s %s\n", turn_address_to_base_4(IC_INIT_VALUE + ic_size + i), turn_line_to_base_4(data_image[i]));
     }
 
     fclose(fp);
     free(obj_file_name);
 }
-
-
 
 void write_ext_file(char *file_name, ExternList *extern_list) {
     FILE *fp;
@@ -164,7 +155,6 @@ void write_ext_file(char *file_name, ExternList *extern_list) {
     /*writing all extern labels with matching addresses */
     current = extern_list->head;
     while (current != NULL) {
-
         /*calculate address in base4*/
         base4_address = turn_address_to_base_4(current->address);
         /*write to extern file extern label name and address */
@@ -175,7 +165,6 @@ void write_ext_file(char *file_name, ExternList *extern_list) {
     fclose(fp);
     free(ext_file_name);
 }
-
 
 void write_ent_file(char *file_name, SymbolTable *symbol_table) {
     FILE *fp;
@@ -216,12 +205,10 @@ void write_ent_file(char *file_name, SymbolTable *symbol_table) {
     current = symbol_table->head;
     while (current != NULL) {
         if (current->is_entry == 1) {
-
             /*calculate address in base 4*/
             base4_address = turn_address_to_base_4(current->address);
             /*write to file entry label name and address*/
             fprintf(fp, "%s %s\n", current->name, base4_address);
-
         }
         current = current->next;
     }
@@ -230,114 +217,35 @@ void write_ent_file(char *file_name, SymbolTable *symbol_table) {
     free(ent_file_name);
 }
 
+void update_code_word(CodeImage *code_image, int address, unsigned int value, char are) {
+    int i;
+    unsigned int final_value = value;
 
-void encode_operand(CodeImage *code_image, int *code_index, char *operand,
-                    SymbolTable *symbol_table, ExternList *extern_list, int current_address) {
-    int addressing_mode = get_addressing_mode(operand);
-    unsigned int encoded_value = 0;
-    char are_field = 'A';
-
-    switch (addressing_mode) {
-        case ADDRESS_IMMEDIATE:
-            /* remove # and convert to integer */
-            encoded_value = atoi(operand + 1);
-            are_field = 'A'; /* absolute */
-            add_code_word(code_image, current_address + *code_index, encoded_value, are_field);
-            (*code_index)++;
-            break;
-
-        case ADDRESS_DIRECT:
-            /* direct label reference */
-            if (is_label_operand(operand)) {
-                Symbol *sym = get_symbol(symbol_table, operand);
-                if (sym != NULL) {
-                    if (sym->type == SYMBOL_EXTERN) {
-                        /* external label */
-                        encoded_value = 0;
-                        /* for external */
-                        are_field = 'E';
-                        /*adding extern label to extern linked list*/
-                        add_extern_symbol(extern_list, operand, current_address + *code_index);
-                    } else {
-                        /* local label */
-                        encoded_value = sym->address;
-                        /* for relocatable */
-                        are_field = 'R';
-                    }
-                }
-            }
-            add_code_word(code_image, current_address + *code_index, encoded_value, are_field);
-            (*code_index)++;
-            break;
-
-        case ADDRESS_MATRIX:
-            /* Matrix addressing: label[row][col] */
-            /* First word: base address of matrix */
-            /* Second word: index calculation */
-            /* This is a simplified implementation - you may need to parse the indices */
-            add_code_word(code_image, current_address + *code_index, 0, 'A');
-            (*code_index)++;
-            add_code_word(code_image, current_address + *code_index, 0, 'A');
-            (*code_index)++;
-            break;
-
-        case ADDRESS_REGISTER:
-            /* Register addressing */
-            encoded_value = identify_register(operand);
-            are_field = 'A'; /* Absolute */
-            add_code_word(code_image, current_address + *code_index, encoded_value, are_field);
-            (*code_index)++;
-            break;
-
-        default:
-            /* Invalid addressing mode */
-            add_code_word(code_image, current_address + *code_index, 0, 'A');
-            (*code_index)++;
-            break;
+    /* Add ARE field to the value */
+    if (are == 'A') {
+        final_value |= 0; /* 00 */
+    } else if (are == 'R') {
+        final_value |= 2; /* 10 */
+    } else if (are == 'E') {
+        final_value |= 1; /* 01 */
     }
+
+    for (i = 0; i < code_image->size; i++) {
+        if (code_image->words[i].address == address) {
+            code_image->words[i].value = final_value;
+            code_image->words[i].ARE = are;
+            return;
+        }
+    }
+
+    printf("ERROR: Could not find word at address %d\n", address);
 }
-
-void encode_instruction(CodeImage *code_image, ParsedLine *parsed, int current_address,
-                        SymbolTable *symbol_table, ExternList *extern_list) {
-    unsigned int encoded_word = 0;
-    int i,code_index = 0;
-    int src_mode = -1, dst_mode = -1;
-
-    /* get addressing modes for operands */
-    if (parsed->operand_count >= 1) {
-        src_mode = get_addressing_mode(parsed->operands[0]);
-    }
-    if (parsed->operand_count >= 2) {
-        dst_mode = get_addressing_mode(parsed->operands[1]);
-    }
-
-    /* encode first word: opcode and addressing modes */
-    encoded_word |= (parsed->opcode << 6); /* opcode in bits 6-9 */
-    if (src_mode != -1) {
-        encoded_word |= (src_mode << 4); /* source mode in bits 4-5 */
-    }
-    if (dst_mode != -1) {
-        encoded_word |= (dst_mode << 2); /* destination mode in bits 2-3 */
-    }
-    /* ARE bits 0-1 are always 'A' for first word */
-
-    /* add first word to code image */
-    add_code_word(code_image, current_address, encoded_word, 'A');
-    code_index = 1;
-
-    /* encode operands */
-    for (i = 0; i < parsed->operand_count; i++) {
-        encode_operand(code_image, &code_index, parsed->operands[i],
-                       symbol_table, extern_list, current_address);
-    }
-}
-
 
 int second_pass(char *am_file, SymbolTable *symbol_table, CodeImage *code_image,
                 int ic_final, int dc_final, unsigned int *data_image) {
     FILE *fp;
     char line[MAX_LINE_LENGTH];
-    int line_number = 0,discover_errors = 0,current_address = IC_INIT_VALUE;
+    int line_number = 0, discover_errors = 0, current_address = IC_INIT_VALUE;
     ParsedLine parsed;
     ExternList extern_list;
 
@@ -357,17 +265,102 @@ int second_pass(char *am_file, SymbolTable *symbol_table, CodeImage *code_image,
 
         /* parse the line */
         if (!parse_line(line, &parsed, am_file, line_number)) {
-            /*no label in the line, move to the next line*/
+            continue;
+        }
+
+        /* Skip empty lines and comments */
+        if (parsed.line_type == LINE_EMPTY || parsed.line_type == LINE_COMMENT) {
             continue;
         }
 
         /* instruction lines */
         if (parsed.line_type == LINE_INSTRUCTION) {
-            /* encode the complete instruction */
-            encode_instruction(code_image, &parsed, current_address, symbol_table, &extern_list);
+            /* Complete encoding of operand words that need symbol resolution */
+            int i,word_offset = 1; /* Skip the first word which is already encoded */
+
+            for ( i = 0; i < parsed.operand_count; i++) {
+                char *operand = parsed.operands[i];
+                int addressing_mode = get_addressing_mode(operand);
+
+                switch (addressing_mode) {
+                    case ADDRESS_DIRECT:
+                        if (is_label_operand(operand)) {
+                            Symbol *sym = get_symbol(symbol_table, operand);
+                            unsigned int encoded_value = 0;
+                            char are_field = 'A';
+
+                            if (sym != NULL) {
+                                if (sym->type == SYMBOL_EXTERN) {
+                                    encoded_value = 0;
+                                    are_field = 'E';
+                                    add_extern_symbol(&extern_list, operand, current_address + word_offset);
+                                } else {
+                                    encoded_value = sym->address;
+                                    are_field = 'R';
+                                }
+                            }
+
+                            update_code_word(code_image, current_address + word_offset, encoded_value, are_field);
+                        }
+                        word_offset++;
+                        break;
+
+                    case ADDRESS_MATRIX: {
+                        /* First word: matrix address */
+                        unsigned int encoded_value = 0;
+                        char are_field = 'A';
+                        char matrix_name[MAX_LABEL_LEN];
+                        Symbol *sym;
+                        sscanf(operand, "%[^[]", matrix_name);
+
+                        sym = get_symbol(symbol_table, matrix_name);
+
+
+                        if (sym != NULL) {
+                            if (sym->type == SYMBOL_EXTERN) {
+                                encoded_value = 0;
+                                are_field = 'E';
+                                add_extern_symbol(&extern_list, matrix_name, current_address + word_offset);
+                            } else {
+                                encoded_value = sym->address;
+                                are_field = 'R';
+                            }
+                        }
+
+                        update_code_word(code_image, current_address + word_offset, encoded_value, are_field);
+                        word_offset += 2; /* Matrix takes 2 additional words */
+                        break;
+                    }
+
+                    case ADDRESS_REGISTER:
+                        /* Check if both operands are registers */
+                        if (parsed.operand_count == 2 &&
+                            get_addressing_mode(parsed.operands[0]) == ADDRESS_REGISTER &&
+                            get_addressing_mode(parsed.operands[1]) == ADDRESS_REGISTER) {
+                            /* Skip the second register since they share a word */
+                            if (i == 0) word_offset++;
+                        } else {
+                            word_offset++;
+                        }
+                        break;
+
+                    case ADDRESS_IMMEDIATE:
+                        word_offset++;
+                        break;
+                }
+            }
 
             /* update current address */
             current_address += instruction_word_count(&parsed);
+        }
+
+        /* Skip data directives */
+        if (parsed.line_type == LINE_DIRECTIVE &&
+            (strcmp(parsed.directive_name, "data") == 0 ||
+             strcmp(parsed.directive_name, "string") == 0 ||
+             strcmp(parsed.directive_name, "mat") == 0 ||
+             strcmp(parsed.directive_name, "extern") == 0)) {
+            continue;
         }
 
         /*.entry directive */
@@ -393,16 +386,11 @@ int second_pass(char *am_file, SymbolTable *symbol_table, CodeImage *code_image,
 
     /* Generate output files if no errors */
     if (!discover_errors) {
-
-        /* creating .ent file containing label name and their address only if exists*/
         write_ent_file(am_file, symbol_table);
-
-        /* creating .ext file containing label name and their address only if exists*/
         write_ext_file(am_file, &extern_list);
         write_code_image_to_ob_file(code_image, ic_final - IC_INIT_VALUE, dc_final, data_image, am_file);
     }
 
     free_extern_list(&extern_list);
-
     return discover_errors;
 }
